b9627af6d7c05e269428ec3f0f3ba867b27d64a8
juan vargas <juanpivl@hotmail.com>
2025-02-21 17:07:06 -0700

    changes

--- src/environments/enviroments.ts
+++ src/environments/enviroments.ts
@@ -2,5 +2,4 @@ export const environment = {
     production: false,
     //apiUrl: 'http://localhost:3000/api/v1/'
     apiUrl: 'http://ec2-3-147-74-107.us-east-2.compute.amazonaws.com:3000/api/v1/'
-
 };
